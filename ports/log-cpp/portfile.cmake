b429e24d4656e11b12c484585fbefc989a6fb6d7
cuiqiusong <cuiqiusong@ovopark.com>
2024-07-16 20:25:18 +0800

    add log-cpp to vcpkg

---
+++ ports/log-cpp/portfile.cmake
@@ -0,0 +1,33 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+# vcpkg_from_gitlab(
+#     GITLAB_URL http://git-inc.ovopark.com:6780
+#     OUT_SOURCE_PATH SOURCE_PATH
+#     REPO system/threadlibrary/log-cpp
+#     REF "${VERSION}"
+#     SHA512 0
+#     HEAD_REF init
+#     AUTHORIZATION_TOKEN Ng2qwPT14ZPo4HwWeJJu
+# )
+
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL http://git-inc.ovopark.com:6780/system/threadlibrary/log-cpp
+    REF ca09d005c62830a57dc876bb1b025fd2adb54433
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    NO_CHARSET_FLAG 
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(
+    CONFIG_PATH lib/cmake/log-cpp
+)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
